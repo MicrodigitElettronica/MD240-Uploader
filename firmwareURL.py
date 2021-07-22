784493142b18b3e26fff06d918795fb10b38d8fd
iw2lsi <iw2lsi@gmail.com>
2021-07-22 03:40:09 -0700

    v1.0.1 - FIX first MA60_Uploader official (Windows) release

--- firmwareURL.py
+++ firmwareURL.py
@@ -1,1 +1,1 @@
-URL = "https://github.com/MicrodigitElettronica/MD240/releases/download/v1.0.0/MA240_full.bin"
+URL = "https://github.com/MicrodigitElettronica/MD240/releases/latest/download/MD240_full.bin"
